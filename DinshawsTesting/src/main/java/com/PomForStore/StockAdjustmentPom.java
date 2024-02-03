747dcc6f777bac0b54d1cf3a991fa6b8a4e21a39
AkshayAlshi <akshayalshi10@gmail.com>
2024-02-03 12:27:16 +0530

    First Commit

--- DinshawsTesting/src/main/java/com/PomForStore/StockAdjustmentPom.java
+++ DinshawsTesting/src/main/java/com/PomForStore/StockAdjustmentPom.java
@@ -1,10 +1,76 @@
 package com.PomForStore;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 import TestBase.Base;
 
 public class StockAdjustmentPom extends Base {
 
 	
 	
+	@FindBy(xpath="(//igx-icon[@class='material-icons igx-icon'])[6]")
+	WebElement StockLocationDrop;
+	
+	@FindBy(xpath="//span[text()=' Main Store ']")
+	WebElement SelectStockLocation;
+	
+	@FindBy(xpath="(//igx-icon[@class='material-icons igx-icon'])[7]")
+	WebElement ItemDrop;
+	
+	
+	@FindBy(xpath="//span[text()=' 9755 - ITEM3 ']")
+	WebElement SelectItem;
+	
+	
+	@FindBy(xpath="//button[text()=' Save ']")
+	WebElement SaveButton;
+	
+	
+	
+	@FindBy(xpath="//button[text()=' Submit ']")
+	WebElement ClickOnSubmitButton;
+	
+	
+	
+	public void StockAdjustmentPom(WebDriver Driver ) {
+		
+		PageFactory.initElements( Driver, this );
+	}
+	
+	public void SelectStockLocation() throws InterruptedException {
+		
+		StockLocationDrop.click();
+		
+		Thread.sleep(500);
+		
+		SelectStockLocation.click();
+	}
+	
+	
+	
+	public void SelectItem() throws InterruptedException {
+		ItemDrop.click();
+		
+		Thread.sleep(500);
+		
+		
+		SelectItem.click();
+		
+	}
+	
+	
+	
+	public void ClickOnSaveButton() {
+		SaveButton.click();
+		
+	}
+	
+	
+	public void ClickOnSubmitButton() {
+		ClickOnSubmitButton.click();
+	}
 	
 }
--- DinshawsTesting/src/test/java/com/TestcaseForStrore/StockAdjustmentTest.java
+++ DinshawsTesting/src/test/java/com/TestcaseForStrore/StockAdjustmentTest.java
@@ -1,5 +1,44 @@
 package com.TestcaseForStrore;
 
-public class StockAdjustmentTest {
+import java.io.IOException;
 
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.PomForStore.StockAdjustmentPom;
+
+import TestBase.Base;
+
+public class StockAdjustmentTest   extends Base  {
+
+	
+	   @BeforeTest
+	    public void setup() throws Exception {
+	        initialization();
+	        Thread.sleep(2000);
+	        selectModuleNameForAdminWithForm(prop.getProperty("ModlueXpath-StockAdjustmentTest"), prop.getProperty("SubModuleXapath-StockAdjustmentTest"), prop.getProperty("FormNameXpath-StockAdjustmentTest"));
+	    }
+
+	    @Test
+	    public void VerifyStockAdjustmentForm() throws InterruptedException, IOException {
+	      
+	    			
+
+	    	StockAdjustmentPom SA = new StockAdjustmentPom();
+	    			
+	    			
+	    			SA.SelectStockLocation();
+	    			SA.SelectItem();
+	    			SA.ClickOnSaveButton();
+	    			SA.ClickOnSubmitButton();
+	     
+	    }
+
+	    @AfterTest
+	    public void tearDown() {
+//	        driver.quit();
+	    }
+	
+	
 }
